9ae079b7722be7060a50e08e44c75042bd1350d9
jayveepcrujido <jvcrujido@gmail.com>
2025-05-16 20:30:55 +0800

    Remove White spaces

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -3,8 +3,8 @@ import unittest
 
 from voter_checker import is_eligible_to_vote
 
-class TestVoterEligibility(unittest.TestCase):
 
+class TestVoterEligibility(unittest.TestCase):
 
     def test_valid_voter(self):
         result = self.is_eligible_to_vote(18)
