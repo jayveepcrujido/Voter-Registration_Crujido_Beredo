64bcbc1327489963ea4a7932fd49fac4e2813b06
jayveepcrujido <jvcrujido@gmail.com>
2025-05-16 20:52:56 +0800

    FIX

--- voter_checker.py
+++ voter_checker.py
@@ -1,6 +1,6 @@
 def is_eligible_to_vote(age, is_citizen):
     if age < 0:
-        raise ValueError("Age cannot be negative")
+        return False
     if age >= 18 and is_citizen:
         return True
     return False
