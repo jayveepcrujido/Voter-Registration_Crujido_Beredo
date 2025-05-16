073497c11fb378898bfc10fb211736008513aae1
jayveepcrujido <jvcrujido@gmail.com>
2025-05-16 20:36:29 +0800

    update test

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -7,22 +7,22 @@ from voter_checker import is_eligible_to_vote
 class TestVoterEligibility(unittest.TestCase):
 
     def test_valid_voter(self):
-        result = self.is_eligible_to_vote(18)
+        result = is_eligible_to_vote(18)
         self.assertTrue(result)
 
     def test_underage_voter(self):
-        result = self.is_eligible_to_vote(17)
+        result = is_eligible_to_vote(17)
         self.assertFalse(result)
 
     def test_non_citizen_voter(self):
-        result = self.is_eligible_to_vote(is_citizen=False)
+        result = is_eligible_to_vote(is_citizen=False)
         self.assertFalse(result)
 
     def test_underage_non_citizen(self):
-        result = self.is_eligible_to_vote(18) \
+        result = is_eligible_to_vote(18) \
             and is_eligible_to_vote(is_citizen=False)
         self.assertFalse(result)
 
     def test_negative_age(self):
-        result = self.is_eligible_to_vote(-1)
+        result = is_eligible_to_vote(-1)
         self.assertEqual(result, "Age cannot be negative")
