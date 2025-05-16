b5b5a747c37c5dc8737bffa3e52d125dfef26ec1
jayveepcrujido <jvcrujido@gmail.com>
2025-05-16 19:54:16 +0800

    initial commit

---
+++ test_voter_checker.py
@@ -0,0 +1,21 @@
+import unittest
+
+from voter_checker import is_eligible_to_vote
+
+class TestVoterEligibility(unittest.TestCase):
+
+    def test_valid_voter(self):
+        self.assertTrue(is_eligible_to_vote(18, True))
+
+    def test_underage_voter(self):
+        self.assertFalse(is_eligible_to_vote(17, False))
+
+    def test_non_citizen_voter(self):
+        self.assertFalse(is_eligible_to_vote(is_citizen = False))
+
+    def test_underage_non_citizen(self):
+        self.assertFalse(is_eligible_to_vote(17, False), is_eligible_to_vote(is_citizen=False))
+
+    
+    def test_negative_age(self):
+        self.assertEqual(is_eligible_to_vote(-1)=="Age cannot be negative")
